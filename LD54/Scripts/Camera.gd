8af0123d495368f24a7b5607b135853129ae0057
0megq <nvatanshenas55@gmail.com>
2023-10-01 11:53:59 -0700

    Better player camera

--- LD54/Scripts/Camera.gd
+++ LD54/Scripts/Camera.gd
@@ -2,6 +2,10 @@ extends Node3D
 	
 var transitioning: bool = false
 
+var lowest_y_pos: float = position.y
+
+var following_player: bool = false
+
 
 func _ready() -> void:
 	set_physics_process(false)
@@ -10,17 +14,24 @@ func _ready() -> void:
 	
 	
 func _physics_process(delta: float) -> void:
-	position.y += delta * 2 / 3
+	lowest_y_pos += delta
+	if following_player:
+		position.y = lerp(position.y, %Player.position.y - 3, 0.1)
+		if abs(position.y - %Player.position.y + 3) < 0.1:
+			following_player = false
+			return
+	
+	if position.y - %Player.position.y + 10 < 0:
+		following_player = true
+	position.y = max(lowest_y_pos, position.y)
 	
 
 func _on_player_edge_detected() -> void:
-	
-
 	%Player.run_direction = %Player.run_direction.rotated(Vector3.UP, PI/2 * sign(%Player.speed))
 	%Player.rotate_y(PI/2 * sign(%Player.speed))
 	
 	var tween = create_tween()
-	tween.tween_property(self, "rotation", rotation + Vector3.UP * PI/2 * sign(%Player.speed), 0.2)
+	tween.tween_property(self, "rotation", rotation + Vector3.UP * PI/2 * sign(%Player.speed), 0.3)
 	tween.set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
 	
 	get_tree().paused = true
@@ -28,5 +39,7 @@ func _on_player_edge_detected() -> void:
 	get_tree().paused = false
 	
 	rotation.y = %Player.rotation.y - PI / 2
+	
+	following_player = position.y < %Player.position.y - 3
 
 	
